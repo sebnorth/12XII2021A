947fddf0dfeab25cd8341b668532ceab374de836
sebnorth <sebnorth@gmail.com>
2021-12-12 15:30:11 +0100

    Dodaj pliki projektów.

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int s = 0;
+
+            while (i<10)
+            {
+                s = s + i;
+                Console.Write("{0} + ", i);
+                i = i + 1;
+                
+            }
+
+            s = s + i;
+            Console.Write("{0} = ", i);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
